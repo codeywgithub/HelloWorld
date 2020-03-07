503fd5d70ed8c01e7208d5a9f41f54048f9b4313
codeywgithub <codey.atkinson@gmail.com>
2020-03-07 11:19:49 +1100

    new changes

--- MyFirstProject/src/helloWorldExample1.java
+++ MyFirstProject/src/helloWorldExample1.java
@@ -3,6 +3,10 @@ public class helloWorldExample1 {
 
 	public static void main(String[] args) {
 		// Output hello world to the user...
+		// Hi there
+		// Hi there
+		// Hi there
+		// Hi there
 		System.out.println("Hello World");
 	}
 
